c30976aade8009ee18505d48fc3e053cf6ad3a22
Roya <royajoker12@gmail.com>
2024-01-08 09:22:48 -0800

    updated

---
+++ product.js
@@ -0,0 +1,46 @@
+const searchInput = document.getElementById("search-input");
+const products = document.querySelectorAll(".product-item");
+const buttons = document.querySelectorAll(".filter")
+
+const changeClass = (filter) =>{ 
+    buttons.forEach(button =>{
+        if(button.detaset.filter === filter){
+            button.classList.add(".selected");
+        } else {
+            button.classList.remove(".selected");
+        }
+    }); 
+};
+const searchHandler = (event) => {
+    const searchValu = event.target.value.toLowerCase().trim();
+
+    products.forEach((product) => {
+     const productName = product.children[1].innerText.toLowerCase();
+     if (productName.includes(searchValu)) {
+        product.style.display = "block";
+     } else {
+        product.style.display = "none";
+     }
+   });
+};
+
+const filterHandler = (event) => {
+    const filter = event.target.dataset.filter;
+    changeClass(filter);
+    products.forEach((product) => {
+        const category =product.dataset.category;
+        if(filter=== "all"){
+            product.style.display = "block";
+        } else {
+           filter=== category ? product.style.display = "block"
+            : product.style.display = "none";
+        }
+    
+    }) 
+};
+
+searchInput.addEventListener("keyup", searchHandler)
+
+buttons.forEach(button => {
+    button.addEventListener("click", filterHandler)
+})
